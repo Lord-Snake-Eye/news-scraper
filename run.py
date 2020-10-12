dffef043b82c21971519d74522257a4052e471fc
sulav <sulavmaharjan63@gmail.com>
2020-10-12 14:45:45 +0545

    Added - File support; removed - opinion category

--- run.py
+++ run.py
@@ -2,21 +2,33 @@ from utils.app import News
 import enquiries
 
 categories = ['Coronavirus', 'Politics', 'Economy',
-              'Society', 'Sports', 'Opinion', 'Lifestyle']
+              'Society', 'Sports', 'Lifestyle']
 choice = enquiries.choose('Choose a category: ', categories)
-recipient = enquiries.freetext('Emails (Use comma to separate emails): ')
-emails = recipient.split(',')
+
+use_file = enquiries.choose('Use file for emails?', ['yes', 'no'])
+
+email_list = []
+
+if use_file == 'yes':
+    with open('emails.txt', 'r') as file:
+        f = file.readlines()
+        for email in f:
+            email_list.append(email.strip())
+else:
+    recipient = enquiries.freetext('Emails (Use comma to separate emails): ')
+    email_list.append(recipient.split(','))
+    email_list = email_list[0]
+
+
 if choice == 'Coronavirus':
-    News(choice).send_mail(emails)
+    News(choice).send_mail(email_list)
 elif choice == 'Politics':
-    News(choice).send_mail(emails)
+    News(choice).send_mail(email_list)
 elif choice == 'Economy':
-    News(choice).send_mail(emails)
+    News(choice).send_mail(email_list)
 elif choice == 'Society':
-    News(choice).send_mail(emails)
+    News(choice).send_mail(email_list)
 elif choice == 'Sports':
-    News(choice).send_mail(emails)
-elif choice == 'Opinion':
-    News(choice).send_mail(emails)
+    News(choice).send_mail(email_list)
 elif choice == 'Lifestyle':
-    News(choice).send_mail(emails)
+    News(choice).send_mail(email_list)
